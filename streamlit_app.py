90cf320b6398d31eebb705332d20612581cdde4e
Hackthecod <indianidol888@gmail.com>
2026-04-13 15:02:29 +0530

    Import pandas and numpy; load dataset from URL
    Added pandas and numpy imports, and loaded penguins dataset.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title('🤖 Stock Price Prediction')
 
 st.info('This is Machine Learning App')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+# df = pd.read_csv(r'penguins_cleaned.csv')
+df
