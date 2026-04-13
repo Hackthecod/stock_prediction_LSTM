9d7b966d0f1da1a6e463d3bea4a5016ffd93dbf9
Hackthecod <indianidol888@gmail.com>
2026-04-13 21:25:53 +0530

    Fix column renaming for prediction probabilities

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,7 +85,7 @@ prediction_prob = clf.predict_proba(input_row)
 
 df_prediction_proba = pd.DataFrame(prediction_prob)
 df_prediction_proba.columns = ['Adelie','Chinstrap','Gentoo']
-df_prediction_prob.rename(columns={0:'Adelie',
+df_prediction_proba.rename(columns={0:'Adelie',
                                 1:'Chinstrap',
                                 2:'Gentoo'})
 
