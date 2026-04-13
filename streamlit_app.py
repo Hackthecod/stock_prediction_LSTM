4713b64f0e383adb946287529899595c48bd50b8
Hackthecod <indianidol888@gmail.com>
2026-04-13 15:03:57 +0530

    Update CSV file loading method in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title('🤖 Stock Price Prediction')
 
 st.info('This is Machine Learning App')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-# df = pd.read_csv(r'penguins_cleaned.csv')
+# df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df = pd.read_csv(r'penguins_cleaned.csv')
 df
