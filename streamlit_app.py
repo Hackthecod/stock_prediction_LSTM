dc609b0005772d09ac19181a540c6d08713702f2
Hackthecod <indianidol888@gmail.com>
2026-04-13 20:23:48 +0530

    Fix flipper length key in dataframe creation

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ with st.sidebar:
     data = {'island':island,
            'bill_length_mm':bill_length_mm,
            'bill_depth_mm':bill_depth_mm,
-           'flipper_length':flipper_length,
+           'flipper_length_mm':flipper_length_mm,
            'body_mass_g':body_mass_g,
            'sex':gender}
     input = pd.DataFrame(data, index=[0])
