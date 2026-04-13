31d9e04d60496c945ad3307ca1221b5a6c5094b8
Hackthecod <indianidol888@gmail.com>
2026-04-13 21:24:50 +0530

    Rename df_prediction_proba to df_prediction_prob
    Fix variable name for prediction probabilities DataFrame.

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,7 @@ clf.fit(X, y)
 prediction = clf.predict(input_row)
 prediction_prob = clf.predict_proba(input_row)
 
-df_prediction_proba = pd.DataFrame(prediction_proba)
+df_prediction_proba = pd.DataFrame(prediction_prob)
 df_prediction_proba.columns = ['Adelie','Chinstrap','Gentoo']
 df_prediction_prob.rename(columns={0:'Adelie',
                                 1:'Chinstrap',
