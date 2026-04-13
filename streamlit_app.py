31ad8f6d79709bb33b31a7f4b21614c0ac0e61f9
Hackthecod <indianidol888@gmail.com>
2026-04-13 15:33:29 +0530

    Change app title and enhance data display
    Updated title and added data display for features and target.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('🤖 Stock Price Prediction')
+st.title('🤖 ML Project')
 
 st.info('This is Machine Learning App')
 
 with st.expander('Data'):
-  st.write("Raw Data")
-  df = pd.read_csv(r'https://raw.githubusercontent.com/Hackthecod/stock_prediction_LSTM/refs/heads/master/penguins_cleaned.csv')
-  df
+    df = pd.read_csv(r'https://raw.githubusercontent.com/Hackthecod/stock_prediction_LSTM/refs/heads/master/penguins_cleaned.csv')
+    df
+
+    st.write('**X**')
+    X = df.drop('species', axis=1)
+    X
+
+    st.write('**X**')
+    y = df['species']
+    y
+
+    st.scatter_chart(X)
